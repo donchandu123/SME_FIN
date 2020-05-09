a53a6aa5a8c154add2c7d6da22560ac435bc56de
donchandu123 <shubhamramtekecjc@gmail.com>
2020-05-09 13:17:41 +0530

    project added

---
+++ src/main/java/com/sme/SmeFinApplication.java
@@ -0,0 +1,13 @@
+package com.sme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmeFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmeFinApplication.class, args);
+	}
+
+}
